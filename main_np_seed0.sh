6870f83f28d73d38a0477c24c200d49d929aa554
Chengxu Zhuang <chengxuz@node06-ccncluster.stanford.edu>
2019-12-27 21:10:55 -0800

    Minor

--- main_np_seed0.sh
+++ main_np_seed0.sh
@@ -19,4 +19,4 @@ mkdir -p ${EXP}
 
 ${PYTHON} main.py ${DIR} --exp ${EXP} --arch ${ARCH} \
   --lr ${LR} --wd ${WD} --k ${K} --sobel --verbose --workers ${WORKERS} \
-  --epochs 500 --seed 33 --resume ${EXP}/checkpoints/checkpoint_35.pth.tar --start_epoch 175
+  --epochs 500 --seed 33 --resume ${EXP}/checkpoints/checkpoint_53.pth.tar --start_epoch 265
